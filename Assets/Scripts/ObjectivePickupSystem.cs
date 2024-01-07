19da6201ac2ef1fe7becb499297e7e3c94c5bb91
Piotr Wendt <wendtpiotr333@gmail.com>
2024-01-07 19:57:07 +0100

    Improving objective system, adding sounds

--- Assets/Scripts/ObjectivePickupSystem.cs
+++ Assets/Scripts/ObjectivePickupSystem.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class ObjectivePickupSystem : MonoBehaviour
 {
@@ -10,24 +11,48 @@ public class ObjectivePickupSystem : MonoBehaviour
     [Space]
     [SerializeField] List<PickupObjective> pickupObjectives = new List<PickupObjective>();
     [SerializeField] string objectiveTag;
+    
+    [SerializeField] Transform cam;
+    [SerializeField, Range(0,10)] int pickupRange; 
+    [SerializeField] AudioSource pickupSound;
+    [SerializeField] TextMeshProUGUI objectivesText;
+    [HideInInspector] GameObject lastFoundObjective;
 
     void Start(){
         for(int i = 0; i < pickupObjectives.Count; i++){
-            pickupObjectives[i].gameObject.AddComponent<Outline>().enabled = false;
-            pickupObjectives[i].gameObject.GetComponent<Outline>().OutlineWidth = 10;
-            pickupObjectives[i].gameObject.GetComponent<Outline>().enabled = true;
+            pickupObjectives[i].gameObject.GetComponent<Outline>().enabled = false;
+            pickupObjectives[i].gameObject.GetComponentInChildren<Canvas>().enabled = false;
+            objectivesText.text = $"{7 - pickupObjectives.Count}/7 OBJECTIVES";
         }
     }
 
-    public void OnCollisionEnter(Collision collider){
-        if(collider.gameObject.tag == objectiveTag){
-            PickupObjective objective = collider.gameObject.GetComponent<PickupObjective>();
-
-            PickupObjective target = pickupObjectives.Find(item => item.id == objective.id);
-            
-            pickupObjectives.Remove(target);
-
-            target.gameObject.SetActive(false);
+    void Update(){
+        RaycastHit hit;
+        GameObject objective;
+        if(Physics.Raycast(cam.transform.position, cam.transform.forward, out hit, pickupRange)){
+            if(hit.transform.gameObject.tag == objectiveTag){
+                if(lastFoundObjective != hit.transform.gameObject){
+                    lastFoundObjective = hit.transform.gameObject;
+                    objective = hit.transform.gameObject;
+                }
+                objective = hit.transform.gameObject;
+                objective.GetComponent<Outline>().enabled = true;
+                objective.GetComponentInChildren<Canvas>().enabled = true;
+                if(Input.GetKeyDown(KeyCode.E)){
+                    PickupObjective pickup = objective.GetComponent<PickupObjective>();
+                    pickupSound.Play();
+                    PickupObjective target = pickupObjectives.Find(item => item.id == pickup.id);
+                    
+                    pickupObjectives.Remove(target);
+                    objectivesText.text = $"{7 - pickupObjectives.Count}/7 OBJECTIVES";
+                    target.gameObject.SetActive(false);
+                }
+            }else{
+                if(lastFoundObjective != null){
+                    lastFoundObjective.GetComponent<Outline>().enabled = false;
+                    lastFoundObjective.GetComponentInChildren<Canvas>().enabled = false;
+                }
+            }
         }
     }
-}
+    }
